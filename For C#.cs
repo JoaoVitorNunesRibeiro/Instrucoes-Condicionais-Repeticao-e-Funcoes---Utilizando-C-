43bc979f8f5d68905376542b2162e4850b1b9f94
JoaoVitorNunesRibeiro <128439201+JoaoVitorNunesRibeiro@users.noreply.github.com>
2023-06-05 21:58:23 -0300

    Update For C#.cs

--- For C#.cs
+++ For C#.cs
@@ -3,12 +3,12 @@ using System;
 class Program {
     static void Main() {
         int num;
-        // interface para o usuário inserir valores inteiros e positivos
+        // pede valores inteiros e positivos para o usuário inserir 
         Console.Write("Digite um número inteiro positivo: ");
         num = int.Parse(Console.ReadLine());
-        // interface da resposta
+        // resposta para o usuário
         Console.Write("Os primeiros " + num + " números pares são: ");
-        // for faz a inicialização, condição e atualização
+        // for tem função de inicialização, condição e atualização
         for (int i = 2; i <= num * 2; i += 2) {
             Console.Write(i + " ");
         }
